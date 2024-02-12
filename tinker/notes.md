23f19efeb8d9640d5bf89b3a82bd0c760f433686
Adam <adamk105@nycstudents.net>
2024-02-12 00:46:32 +0000

    update repo

---
+++ tinker/notes.md
@@ -0,0 +1,17 @@
+Hitting
+
+Batting avg - Hits / At-bats = 3 decimals
+
+On-base Percentage - (Hits + Walks + Hit By Pitch) / (At-bats + Walks + Hit By Pitch + Sacrifices) = 3 decimals
+
+Slugging Percentage - Total number of bases / At-bats
+
+On-base plus Slugging - On-base percentage + Slugging percentage
+
+
+Pitching
+
+Earned Run Average - (Earned Runs Allowed divided by Innings Pitched) * 9
+
+
+(https://www.mlbbaseballfreepicks.com/calculating-baseball-stats/)
--- tinker/script.js
+++ tinker/script.js
@@ -72,54 +72,125 @@
 //   .attr("stroke-width", 1)
 //   .attr("d", line);
 
-const data = [
-  { name: 'Adam', score:92 },
-  { name: 'Shelly', score:55 },
-  { name: 'Mueller', score:77 },
-  { name: 'Daniel', score:100 },
-  { name: 'Dong', score:65 },
-  { name: 'William', score:50 },
-  { name: 'Stefanie', score:84 },
+// const data = [
+//   { name: 'Adam', score:92 },
+//   { name: 'Shelly', score:55 },
+//   { name: 'Mueller', score:77 },
+//   { name: 'Daniel', score:100 },
+//   { name: 'Dong', score:65 },
+//   { name: 'William', score:50 },
+//   { name: 'Stefanie', score:84 },
+// ];
+
+// const width = 800;
+// const height = 400;
+// const margin = { top: 50, bottom: 50, left: 50, right:50};
+
+// const svg = d3.select('#chart-container')
+//   .append('svg')
+//   .attr('height', height - margin.top - margin.bottom)
+//   .attr('width', margin - margin.top - margin.bottom)
+//   .attr('viewbox', [0, 0, width, height]);
+
+// const x = d3.scaleBand()
+//   .domain(d3.range(data.length))
+//   .range([margin.left, width - margin.right])
+//   .padding(0.05);
+
+// const y = d3.scaleLinear()
+//   .domain([0, 100])
+//   .range([height - margin.bottom, margin.top])
+
+//   svg
+//     .append('g')
+//     .attr('fill', 'royalblue')
+//     .selectAll('rect')
+//     .data(data.sort((a, b) => d3.descending(a.score, b.score)))
+//     .join('rect')
+//       .attr('x', (d, i)=> x(i))
+//       .attr('y', (d)=> y(d.score))
+//       .attr('height', d=> y(0) - y(d.score))
+//       .attr('width', x.bandwidth())
+
+// function xAxis(g){
+//   g.attr('transform', `translate(0, ${height - margin.bottom})`)
+//   .call(d3.axisBottom(x).tickFormat(i => data[i].name))
+// }
+
+// function yAxis(g){
+
+// }
+
+// svg.append('g').call(xAxis);
+// svg.node();
+
+// svg.append("text")
+//   .attr("class", "x label")
+//   .attr("text-anchor", "end")
+//   .attr("x", width)
+//   .attr("y", height - 6)
+//   .text("NAMES");
+
+// svg.append("text")
+//   .attr("class", "y label")
+//   .attr("text-anchor", "end")
+//   .attr("y", height - 375)
+//   .attr("tranform", "translate(-40, 40)")
+//   .attr("dy", ".75em")
+//   .attr("transform", "rotate(-90)")
+//   .text("SCORES (/100)");
+
+
+
+
+// var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+// var svgWidth = 500, svgHeight = 300, barPadding = 5;
+// var barWidth = (svgWidth / dataset.length);
+
+// var svg = d3.select('svg')
+//   .attr("width", svgWidth)
+//   .attr("height", svgHeight);
+
+// var barChart = svg.selectAll("rect")
+//   .data(dataset)
+//   .enter()
+//   .append("rect")
+//   .attr("y", function(d) {
+//     return d;
+//   })
+//   .attr("width", barWidth - barPadding)
+//   .attr("transform", function(d, i) {
+//     var translate = [barWidth * i, 0];
+//     return "translate("+ translate +")";
+//   });
+
+
+var data = [
+  {"platform": "Android","percentage": 40.11},
+  {"platform": "Windows","percentage": 36.69},
+  {"platform": "iOS","percentage": 13.06}
 ];
 
-const width = 800;
-const height = 400;
-const margin = { top: 50, bottom: 50, left: 50, right:50};
+var svgWidth = 500, svgHeight = 300, radius = Math.min(svgWidth, svgHeight) / 2;
+var svg = d3.select('svg')
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
-const svg = d3.select('#chart-container')
-  .append('svg')
-  .attr('height', height - margin.top - margin.bottom)
-  .attr('width', margin - margin.top - margin.bottom)
-  .attr('viewbox', [0, 0, width, height]);
+var g = svg.append("g")
+  .attr("transform", "translate(" + radius + "," + radius + ")");
 
-  const x = d3.scaleBand()
-    .domain(d3.range(data.length))
-    .range([margin.left, width - margin.right])
-    .padding(0.1);
+var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-const y = d3.scaleLinear()
-  .domain([0, 100])
-  .range([height - margin.bottom, margin.top])
+var pie = d3.pie().value(function(d) {
+  return d.percentage;
+});
 
-  svg
-    .append('g')
-    .attr('fill', 'royalblue')
-    .selectAll('rect')
-    .data(data.sort((a, b) => d3.descending(a.score, b.score)))
-    .join('rect')
-      .attr('x', (d, i)=> x(i))
-      .attr('y', (d)=> y(d.score))
-      .attr('height', d=> y(0) - y(d.score))
-      .attr('width', x.bandwidth())
+var path = d3.arc()
+  .outerRadius(radius)
+  .innerRadius(0);
 
-function xAxis(g){
-  g.attr('transform', `translate(0, ${height - margin.bottom})`)
-  .call(d3.axisBottom(x).tickFormat(i => data[i].name))
-}
-
-function yAxis(g){
-
-}
-
-svg.append('g').call(xAxis);
-svg.node();
+var arc = g.selectAll("arc")
+  .data(pie(data))
+  .enter()
+  .append("g");
